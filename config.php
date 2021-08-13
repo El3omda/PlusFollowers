26efce798bd17655925c31b89b99986e596d9a62
El3omda <52126083+El3omda@users.noreply.github.com>
2021-08-13 10:24:26 +0200

    Create Connection File

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+// Start Connection To DataBase
+
+# Connection Variables
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'instafollowers';
+
+# Conn
+
+@$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+if (!$conn) {
+  echo 'Connection Faild Cause -> ' . mysqli_connect_error();
+}
+
+header("Location:index.php");
